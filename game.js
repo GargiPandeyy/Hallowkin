818eef64a5bd9190968ad938ba3558eeb65b7147
GargiPandeyy <computercomcon@gmail.com>
2025-10-31 05:30:16 +0530

    create pumpkin falling

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ let gameArea = document.getElementById('gameArea');
 let basketX = 350;
 let basketWidth = 60;
 let gameWidth = 800;
+let pumpkins = [];
+let fallSpeed = 3;
 
 document.addEventListener('keydown', function(e) {
     if(e.key === 'ArrowLeft' && basketX > 0) {
@@ -22,3 +24,29 @@ document.addEventListener('mousemove', function(e) {
         basket.style.left = basketX + 'px';
     }
 });
+
+function createPumpkin() {
+    let pumpkin = document.createElement('div');
+    pumpkin.className = 'pumpkin';
+    pumpkin.innerHTML = '🎃';
+    pumpkin.style.left = Math.random() * (gameWidth - 50) + 'px';
+    pumpkin.style.top = '-50px';
+    gameArea.appendChild(pumpkin);
+    pumpkins.push({element: pumpkin, y: -50});
+}
+
+function gameLoop() {
+    for(let i = pumpkins.length - 1; i >= 0; i--) {
+        pumpkins[i].y += fallSpeed;
+        pumpkins[i].element.style.top = pumpkins[i].y + 'px';
+
+        if(pumpkins[i].y > 600) {
+            pumpkins[i].element.remove();
+            pumpkins.splice(i, 1);
+        }
+    }
+    requestAnimationFrame(gameLoop);
+}
+
+setInterval(createPumpkin, 1500);
+gameLoop();
